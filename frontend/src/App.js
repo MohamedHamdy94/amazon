951e1d0fa2d7bf21492bc0a0c75e302ffea167a7
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2022-03-16 09:59:42 +0200

    add

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          amazon
+        </a>
+      </header>
+      <main>list</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,5 @@
+const data={
+    product:[
+        name:;
+    ]
+}
